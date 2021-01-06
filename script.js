b6b6e953ef6f40e65c936ca8c0405d202f3ca3fc
Luis <m.luishernandez@outlook.com>
2021-01-06 10:16:52 -0500

    added to local storage

--- script.js
+++ script.js
@@ -1,72 +1,57 @@
 $(document).ready(function(){
     $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
-    var currentTime = moment().format('LT');
     
+    var currentTime = parseInt(moment().format("H"));
 
-    
+    console.log(currentTime);
    
     displayBlocks();
     function displayBlocks(){
 
-        for (var i = 0; i < 9; i++){
+        for (var i = 9; i < 18; i++){
 
-            //3rd Task
-            // The time blocks need to be color coded depending on the hour
-            // How can we account for time
-            // if its past gray, present red and future green
+        var divBlock = $("<div>");
+        $(divBlock).appendTo(".container");
+        divBlock.addClass("row time-block");
 
-            
-
-        //1. Rows for each individual hour (9)
-        var divRow = $("<div>");
-        $(divRow).appendTo(".container");
-        divRow.addClass("row time-block");
         //2. Columns for displaying the hours 
-            
         var hourcol = $("<div>");
-        $(hourcol).appendTo(divRow);
-        hourcol.addClass("col-md-1 p-2 hour ");
-        //targeting the current day basically we need to target the individual hour block
-        //if i is less than 3 text i which is 0 plus 9 which is 2++
-        
-        if(i <3){
-            hourcol.text(i + 9 + "am")
-        }
-        //if i is equal to 3 then text i which is 0 and 9
-        else if (i === 3){
-            hourcol.text(i + 9 +"pm")
-        }
-        //if i is more than 3 then
-        else{
-            hourcol.text(-3 + i +"pm")
-        }
+        $(hourcol).appendTo(divBlock);
+        hourcol.addClass("col-md-1 p-2 hour");
+        hourcol.attr("id", "hour-" + i);
+        hourcol.text(moment(i, "H").format("h a"));
 
         //3. Textarea column so it can recieve input from the user
-        var userInput = $("<textarea>");
-        $(userInput).appendTo(divRow);
-        userInput.addClass("col-md-10 textarea past");
-        userInput.text(currentTime).empty();
-        console.log(currentTime);
+        var textArea = $("<textarea>");
+        $(textArea).appendTo(divBlock);
+        textArea.attr("id", "area");
+        textArea.addClass("col-md-10 textarea" );
         
-        if (currentTime < ){
-            
-            //we need to compare the current time to present hours
-            //we need to compare the current time to past hours
-            //textarea add attribute to the one needed on the parameter
+        let timeClass = "past"
+	    if (currentTime === i) timeClass = "present"
+        else if (currentTime < i) timeClass = "future"
+        textArea.addClass(timeClass)
 
-        }
-        
-        
         //4. Columns on the left sign with save buttons
         var storageBtn = $("<button>");
-        $(storageBtn).appendTo(divRow);
+        $(storageBtn).appendTo(divBlock);
         storageBtn.addClass("saveBtn col-md-1 far fa-save");
-
         
-
-
-
         }
+        //Todo - Change this to target the document first because the saveBtn is dynamic.
+
+        $(".saveBtn").on("click", function(){
+            // This is getting the value from the text area
+           var userInput = $(this).siblings("#area").val();
+           // This is  getting the hour class and adding the attr id
+          var timeText =  $(this).siblings(".hour").attr("id");
+
+            localStorage.setItem(timeText, userInput);
+            
+          });
     }
+    // Need to update html with the values from each item of local storage
+    //Get Item
+      
 })
